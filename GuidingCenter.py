3f539b196c5c3295b1a01063ce8fc95676cd466f
Kaan Öztürk <mkozturk@yahoo.com>
2016-01-12 19:48:52 -0600

    Stores parallel momentum instead of parallel speed. Interface changed to accept pitch angle or parallel momentum. Initializers modified.

--- GuidingCenter.py
+++ GuidingCenter.py
@@ -11,76 +11,95 @@ import rapt.utils as ru
 from rapt import c, params, Particle
 
 class GuidingCenter:
-    # TO DO: Not compatible with electric fields and time-varying magnetic fields.
-    # The advance() method needs to have:
-    #   (1) The ExB term
-    #   (2) The speed as a separate variable that's updated in time.
-    #       Update the kinetic energy at each step and get speed from it.
-    def __init__(self, pos=None, vel=None, t0=None, mass=None, charge=None, field=None):
+    def __init__(self, pos=None, v=None, pa=None, ppar=None, t0=0, mass=None, charge=None, field=None):
+        # pos: initial position (array of 3)
+        # v: initial speed (number)
+        # pa: initial pitch angle
+        # ppar = initial parallel momentum
+        #     (N.B. Either pa or ppar should be provided. If both given, pa is used.)
+        # t0: initial time
+        # mass: particle mass in kg
+        # charge: particle charge in C
+        # field: The field object
+    
         self.pos = pos  # initial position array
-        tpos = np.concatenate([[t0], pos])        
-        self.vp = np.dot(vel, field.B(tpos)) / field.magB(tpos)  # initial parallel speed
-        self.v = np.sqrt(np.dot(vel,vel))    # speed of the particle
+        self.v = v
+        if pa != None:
+            gamma = 1/np.sqrt(1-(v/c)**2)
+            ppar = gamma * mass * v * np.cos(pa*np.pi/180)
+        
         self.t0 = t0 # initial time
         self.tcur = t0    # current time
         self.mass = mass  # mass of the particle
         self.charge = charge  # charge of the particle
         self.field = field
-        if not (pos==None or vel==None or t0==None): # if initial state is given explicitly
-            self.mu = ru.magnetic_moment(self.tcur, self.pos, self.vp, self.v, self.field, self.mass)
-            self.trajectory = np.concatenate(([t0], pos, [self.vp]))
+        if not (pos==None or v==None or ppar==None): # if initial state is given explicitly
+            self.mu = ru.magnetic_moment(self.tcur, self.pos, ppar/gamma/mass,
+                                         self.v, self.field, self.mass)
+            self.trajectory = np.concatenate(([t0], pos, ppar))
             self.trajectory = np.reshape(self.trajectory, (1,5))
     
     def init(self, p):  # Initialization with another Particle or GuidingCenter instance
         if isinstance(p, GuidingCenter):
-            self.tcur = p.trajectory[-1,0]            
-            self.pos = p.trajectory[-1,1:4]
-            self.vp = p.trajectory[-1,4]
-            self.v = p.v 
-            self.mass = p.mass
-            self.charge = p.charge
-            self.field = p.field
-            self.mu = ru.magnetic_moment(self.tcur, self.pos, self.vp, self.v, self.field, self.mass)
-            self.trajectory = np.concatenate(([self.tcur], self.pos, [self.vp]))
-            self.trajectory = np.reshape(self.trajectory, (1,5))
+            B = p.field.magB(p.trajectory[-1,:4])            
+            gammasq = 1 + 2*p.mu*B/(p.mass*c*c) + (p.trajectory[-1,4]/p.mass/c)**2
+            v = c * np.sqrt(1-1/gammasq)
+            self.__init__(pos = p.trajectory[-1,1:4],
+                          v = v,
+                          ppar = p.trajectory[-1,4],
+                          t0 = p.trajectory[-1,0],
+                          mass = p.mass, 
+                          charge = p.charge,
+                          field = p.field)
+                
         elif isinstance(p, Particle):
-            self.mass = p.mass
-            self.charge = p.charge
-            self.field = p.field
-            self.tcur = p.trajectory[-1,0]
-            self.pos, self.vp, self.v = ru.guidingcenter(self.tcur, p.trajectory[-1,1:4], p.trajectory[-1,4:], self.field, self.mass, self.charge)
-            self.mu = ru.magnetic_moment(self.tcur, self.pos, self.vp, self.v, self.field, self.mass)
-            self.trajectory = np.concatenate(([self.tcur], self.pos, [self.vp]))
-            self.trajectory = np.reshape(self.trajectory, (1,5))
+            pos, vp, v = ru.guidingcenter(p.trajectory[-1,0],
+                                          p.trajectory[-1,1:4],
+                                          p.trajectory[-1,4:], 
+                                          p.field, 
+                                          p.mass,
+                                          p.charge)
+            gamma = 1 / np.sqrt(1-(v/c)**2)
+            self.__init__(pos = pos,
+                          v = v,
+                          ppar = p.mass*gamma*vp,
+                          t0 = p.trajectory[-1,0],
+                          mass = p.mass, 
+                          charge = p.charge,
+                          field = p.field)
         else:
             raise(ValueError, "Particle or GuidingCenter objects required.")
-            
-    def setpa(self, pa):
-        """Resets the velocity vector so that the pitch angle is pa degrees. Reinitializes the object."""
-        field = self.field
-        tpos = self.trajectory[-1,0:4]
-        v = self.trajectory[-1,4:] # velocity
-        
-        # Construct a vector that has the same length as v,
-        # lies on the plane spanned by v and B,
-        # and makes an angle of pa with B.
-        
-        # the unit vector in the direction of the field.
-        b = field.unitb(tpos)
-        # component of v perpendicular to b:
-        vperp = v - np.dot(v,b)*b
-        # unit vector of vperp
-        p = vperp / np.sqrt(np.dot(vperp,vperp))
-        # speed
-        s = np.sqrt(np.dot(v,v))
-        # The new velocity vector
-        w = s*np.sin(pa*np.pi/180)*b + s*np.cos(pa*np.pi/180)*p
-        # Reinitialize with the new velocity
-        self.__init__(self.pos, w, self.t0, self.mass, self.charge, self.field)
+
+#   Not used anymore:            
+#    def setpa(self, pa):
+#        """Resets the velocity vector so that the pitch angle is pa degrees. Reinitializes the object."""
+#        field = self.field
+#        tpos = self.trajectory[-1,0:4]
+#        v = self.trajectory[-1,4:] # velocity
+#        
+#        # Construct a vector that has the same length as v,
+#        # lies on the plane spanned by v and B,
+#        # and makes an angle of pa with B.
+#        
+#        # the unit vector in the direction of the field.
+#        b = field.unitb(tpos)
+#        # component of v perpendicular to b:
+#        vperp = v - np.dot(v,b)*b
+#        # unit vector of vperp
+#        p = vperp / np.sqrt(np.dot(vperp,vperp))
+#        # speed
+#        s = np.sqrt(np.dot(v,v))
+#        # The new velocity vector
+#        w = s*np.sin(pa*np.pi/180)*b + s*np.cos(pa*np.pi/180)*p
+#        # Reinitialize with the new velocity
+#        self.__init__(self.pos, w, self.t0, self.mass, self.charge, self.field)
 
     def isadiabatic(self):
         """Returns True if the particle's motion satisfies the adiabaticity conditions
         at the present location."""
+        # IMPROVEMENT: Functions cycrad and cycper repeat the calcuation of v.
+        # Evaluate here directly, reusing v values.
+        
         # Spatial and temporal adiabaticity thresholds
         eps_sp = params['epss']
         eps_t = params['epst'] 
@@ -96,15 +115,16 @@ class GuidingCenter:
             and self.cycper() / self.field.timescale(self.trajectory[-1,:4]) < eps_t
     
     def advance(self, delta):
-        if self.field.isstatic:
-            if params["GCeq"] == "northropteller":
-                self.NorthropTellerAdvance(delta)
-            elif params["GCeq"] == "brizardchan":
-                self.BrizardChanAdvance(delta)
-            else:
-                raise Exception("Guiding-center equation "+params["GCeq"]+" not implemented.")
-        else:
-            self.TaoChanBrizardAdvance(delta)
+        self.TaoChanBrizardAdvance(delta)
+#        if self.field.isstatic:
+#            if params["GCeq"] == "northropteller":
+#                self.NorthropTellerAdvance(delta)
+#            elif params["GCeq"] == "brizardchan":
+#                self.BrizardChanAdvance(delta)
+#            else:
+#                raise Exception("Guiding-center equation "+params["GCeq"]+" not implemented.")
+#        else:
+#            self.TaoChanBrizardAdvance(delta)
     
     def TaoChanBrizardAdvance(self, delta):
         # To be used when electric fields are nonzero and/or fields vary in time.
@@ -117,7 +137,7 @@ class GuidingCenter:
             B = self.field.B(Y[:4])
             Bmag = np.sqrt(np.dot(B,B))
             unitb = B / Bmag
-            gamma = np.sqrt(1 + self.mu*Bmag/(m*c*c) + (Y[4]/(m*c)**2))
+            gamma = np.sqrt(1 + 2*self.mu*Bmag/(m*c*c) + (Y[4]/(m*c)**2))
             cb = self.field.curlb(Y[:4])
             Bstar = B + Y[4] * cb / self.charge
             Bstarpar = np.dot(Bstar,unitb)
@@ -204,7 +224,14 @@ class GuidingCenter:
     def getz(self):
         return self.trajectory[:,3]
     def getvp(self):
-        return self.trajectory[:,4]
+        return self.trajectory[:,4]  # parallel momentum
+#        if self.field.isstatic:
+#            return self.trajectory[:,4]
+#        else:
+#            # The nonstatic case stores the parallel momentum. Convert to parallel speed.
+#            B = self.getB()
+#            gamma = np.sqrt( 1 + 2*self.mu*B/(self.mass*c*c) + (self.trajectory[:,4]/self.mass/c)**2)
+#            return self.trajectory[:,4]/gamma/self.mass
     def getr(self):  # Distance from the origin
         return np.sqrt( self.getx()**2 + self.gety()**2 + self.getz()**2 )
     def gettheta(self): # Polar angle
@@ -220,12 +247,26 @@ class GuidingCenter:
     def cycrad(self):
         """The current cyclotron radius."""
         t, r, vp = self.trajectory[-1, 0], self.trajectory[-1, 1:4], self.trajectory[-1, 4]
-        return ru.cyclotron_radius2(t, r, vp, self.v, self.field, self.mass, self.charge)
+        if self.field.isstatic:
+            return ru.cyclotron_radius2(t, r, vp, self.v, self.field, self.mass, self.charge)
+        else:            
+            # The advancer stores parallel momentum at last element. Convert to parallel speed.
+            Bmag = self.field.magB(self.trajectory[-1,:4])
+            gamma = np.sqrt(1 + 2*self.mu*Bmag/(self.mass*c*c) + (vp/(self.mass*c)**2))
+            vp = vp / self.mass / gamma # parallel speed
+            v = c * np.sqrt(1-1/gamma**2)
+            return ru.cyclotron_radius2(t, r, vp, v, self.field, self.mass, self.charge)
 
     def cycper(self):
         """The current cyclotron period."""
         t, r = self.trajectory[-1, 0], self.trajectory[-1, 1:4]
-        return ru.cyclotron_period2(t, r, self.v, self.field, self.mass, self.charge)
+        if self.field.isstatic:
+            return ru.cyclotron_period2(t, r, self.v, self.field, self.mass, self.charge)
+        else:
+            Bmag = self.field.magB(self.trajectory[-1,:4])
+            gamma = np.sqrt(1 + 2*self.mu*Bmag/(self.mass*c*c) + (vp/(self.mass*c)**2))
+            v = c * np.sqrt(1-1/gamma**2)
+            return ru.cyclotron_period2(t, r, v, self.field, self.mass, self.charge)
 
     def ke(self):
         # BUG: Drift speed term not added.
